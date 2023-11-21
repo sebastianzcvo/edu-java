783a9581729f567eba066790b5af08ce3fda1238
sbszc <sebastian.zcc@gmail.com>
2023-11-20 21:36:02 -0500

    oop update

---
+++ src/main/java/oop/abstractClasses/SayHi.java
@@ -0,0 +1,7 @@
+package oop.abstractClasses;
+
+// just like a normal class, but can't be used to create instances from: abstract classes are meant to be extended
+// can contain abstract methods: instance methods that have to be overriden
+public abstract class SayHi {
+    abstract void method(); // 'private, final, static, synchronized' don't make sense on abstracts methods
+}
