07409f7b8a2bc772abe612840b39d31556ddc232
sbszc <sbs.zc@hotmail.com>
2025-09-27 18:24:29 -0500

    access modifier update

--- src/main/java/accessModifiers/a/A.java
+++ src/main/java/accessModifiers/a/A.java
@@ -2,7 +2,7 @@ package accessModifiers.a;
 
 public class A {
     static private String privateProp = "";
-    static String defaultProp = ""; // packageprivate
+    static String defaultProp = ""; // package-private
     static protected String protectedProp = ""; // package-private + subclasses
     static public String publicProp = "";
 }
